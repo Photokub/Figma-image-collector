4cd7c50d8e46a7d7386417bf505a82747c1a1f66
A_Kub_work <photokub@yandex.ru>
2023-04-28 12:04:03 +0300

    Feat: логипа параллельной обработки массивов PLU и LINKS

--- src/code.ts
+++ src/code.ts
@@ -5,13 +5,10 @@ figma.ui.onmessage = pluginMessage => {
 
     (async () => {
 
-        //const postComponentSet = figma.root.findOne(node => node.type == "COMPONENT_SET" && node.name == "post") as ComponentSetNode
-
         const pluArr = pluginMessage.pluArr;
         const linkArr = pluginMessage.linkArr;
         console.log(pluArr)
         console.log(linkArr)
-
 
         const link = await figma.createText()
         await figma.loadFontAsync({family: "Inter", style: "Regular"})
@@ -21,37 +18,15 @@ figma.ui.onmessage = pluginMessage => {
         link.fontSize = 18
         link.fills = [{type: 'SOLID', color: {r: 0, g: 0, b: 0}}]
 
-        const imageLink = await figma.createImageAsync(
-            pluginMessage.linkValue
-        )
-
-        const imageContainer = figma.createRectangle()
-        //const {width, height} = await imageLink.getSizeAsync()
-        imageContainer.resize(475, 400)
-        imageContainer.fills = [
-            {
-                type: 'IMAGE',
-                imageHash: imageLink.hash,
-                scaleMode: 'FIT'
-            }
-        ]
-
-        pluArr.forEach((elem) => {
-            const frame = figma.createFrame();
-            frame.x = 50
-            frame.y = 50
-            frame.resize(475, 400)
-            frame.name = elem
-            frame.appendChild(imageContainer)
-        })
-
-
         /////////////синхронный запрос///////////////
-        let array = linkArr[Symbol.iterator]();
-        let arr = Array.from(array);
-        console.log(arr)
+        let iterLinksArray = linkArr[Symbol.iterator]();
+        let iterPluArray = pluArr[Symbol.iterator]();
+        let linksArray = Array.from(iterLinksArray);
+        let pluArray = Array.from(iterPluArray);
+        console.log(linksArray)
+        console.log(pluArray)
 
-        for (let elem of arr) {
+        for (let elem of linksArray) {
             if (typeof elem === "string") {
                 console.log(elem)
                 let imgLink = await figma.createImageAsync(elem)
@@ -67,68 +42,20 @@ figma.ui.onmessage = pluginMessage => {
                     }
                 ]
 
-                //pluArr.forEach((elem) => {
-                    const frame = figma.createFrame();
-                    frame.x = 50
-                    frame.y = 50
-                    frame.resize(475, 400)
-                    frame.name = elem
-                    frame.appendChild(rectangleObject)
-                //})
+                const frame = figma.createFrame();
+                frame.x = 50
+                frame.y = 50
+                frame.resize(475, 400)
+                let currentIndex = linksArray.indexOf(elem);
+                console.log(currentIndex)
+                let currentFrameName = pluArray[currentIndex]
+                console.log(currentFrameName)
+                if (typeof currentFrameName === "string") {
+                    frame.name = currentFrameName
+                }
+                frame.appendChild(rectangleObject)
             }
         }
-
-
-        //todo /////////////асинхронный запрос///////////////
-        // let asyncArray = linkArr;
-        // //let asyncArray = linkArr[Symbol.asyncIterator]();
-        // let asyncArr = Array.from(asyncArray);
-        // //console.log(asyncArr);
-        //
-        // let generatorArray = function* (arr) {
-        //     let i = 0
-        //     let length = arr.length
-        //
-        //     while (true) {
-        //         yield arr[i]
-        //         i++
-        //         if (i === length) i = 0
-        //     }
-        // };
-        //
-        // // await (async () => {
-        //     const arrayLoop = generatorArray(asyncArr)
-        //     for await (let value of arrayLoop) {
-        //         console.log(arrayLoop.next())
-        //         if (typeof value === "string") {
-        //             console.log(value)
-        //             let imgLink = await figma.createImageAsync(value)
-        //             console.log(imgLink)
-        //
-        //             const rect = figma.createRectangle()
-        //             imageContainer.resize(475, 400)
-        //             imageContainer.fills = [
-        //                 {
-        //                     type: 'IMAGE',
-        //                     imageHash: imgLink.hash,
-        //                     scaleMode: 'FIT'
-        //                 }
-        //             ]
-        //
-        //             pluArr.forEach((elem) => {
-        //                 const frame = figma.createFrame();
-        //                 frame.x = 50
-        //                 frame.y = 50
-        //                 frame.resize(475, 400)
-        //                 frame.name = elem
-        //                 frame.appendChild(rect)
-        //             })
-        //         }
-        //     }
-        //     ;
-        //
-        // // })();
-
 
         await figma.closePlugin()
     })()
