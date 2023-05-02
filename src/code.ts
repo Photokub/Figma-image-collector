edc97ed513d23dacad6f64d3f904be2b0f4c6b73
A_Kub_work <photokub@yandex.ru>
2023-05-02 18:49:25 +0300

    Feat: демонстрация пользователю ошибки API createImageAsync

--- src/code.ts
+++ src/code.ts
@@ -22,48 +22,79 @@ figma.ui.onmessage = pluginMessage => {
 
         for (let elem of linksArray) {
             if (typeof elem === "string") {
-                console.log(elem)
-                let imgLink = await figma.createImageAsync(elem)
-                if (!imgLink){
-                    throw new Error('ОШИБКА ОШИБКА!!! ЭТО НЕ ИЗОБРАЖЕНИЕ')
-                    console.log('ОШИБКА ОШИБКА!!! ЭТО НЕ ИЗОБРАЖЕНИЕ')
-                }
-                console.log(imgLink)
-                const rectangleObject = figma.createRectangle()
-                rectangleObject.resize(395, 320)
-                rectangleObject.fills = [
-                    {
-                        type: 'IMAGE',
-                        imageHash: imgLink.hash,
-                        scaleMode: 'FIT'
-                    }
-                ]
+                try{
+                    console.log(elem)
+                    let imgLink = await figma.createImageAsync(elem)
+                    // if (!imgLink){
+                    //     throw new Error('ОШИБКА ОШИБКА!!! ЭТО НЕ ИЗОБРАЖЕНИЕ')
+                    //     console.log('ОШИБКА ОШИБКА!!! ЭТО НЕ ИЗОБРАЖЕНИЕ')
+                    // }
+                    console.log(imgLink)
+                    const rectangleObject = figma.createRectangle()
+                    rectangleObject.resize(395, 320)
+                    rectangleObject.fills = [
+                        {
+                            type: 'IMAGE',
+                            imageHash: imgLink.hash,
+                            scaleMode: 'FIT'
+                        }
+                    ]
 
-                const frame = figma.createFrame();
-                frame.x = 50
-                frame.y = 50
-                frame.resize(475, 400)
-                let currentIndex = linksArray.indexOf(elem);
-                console.log(currentIndex)
-                let currentFrameName = pluArray[currentIndex]
-                console.log(currentFrameName)
-                if (typeof currentFrameName === "string") {
-                    frame.name = currentFrameName
+                    const frame = figma.createFrame();
+                    frame.x = 50
+                    frame.y = 50
+                    frame.resize(475, 400)
+                    let currentIndex = linksArray.indexOf(elem);
+                    console.log(currentIndex)
+                    let currentFrameName = pluArray[currentIndex]
+                    console.log(currentFrameName)
+                    if (typeof currentFrameName === "string") {
+                        frame.name = currentFrameName
+                    }
+                    frame.layoutMode = 'HORIZONTAL'
+                    frame.horizontalPadding = 40
+                    frame.counterAxisAlignItems = 'CENTER'
+                    frame.appendChild(rectangleObject)
+                }catch(err){
+                    // const errorObject = figma.createRectangle()
+                    // errorObject.resize(395, 320)
+                    // errorObject.fills = [{ type: 'SOLID', color: { r: 1, g: 0, b: 0 } }]
+
+                    const errorText = await figma.createText()
+                    await figma.loadFontAsync({family: "Inter", style: "Regular"})
+                    errorText.resize(395, 320)
+                    errorText.x = 50
+                    errorText.y = 0
+                    errorText.characters = ('ERROR:' + '\n' + (err))
+                    errorText.fontSize = 24
+                    errorText.fills = [{type: 'SOLID', color: {r: 1, g: 0, b: 0}}]
+
+                    const frame = figma.createFrame();
+                    frame.x = 50
+                    frame.y = 50
+                    frame.resize(475, 400)
+                    let currentIndex = linksArray.indexOf(elem);
+
+                    let currentFrameName = pluArray[currentIndex]
+
+                    if (typeof currentFrameName === "string") {
+                        frame.name = currentFrameName
+                    }
+                    frame.layoutMode = 'HORIZONTAL'
+                    frame.horizontalPadding = 40
+                    frame.counterAxisAlignItems = 'CENTER'
+                    frame.appendChild(errorText)
                 }
-                frame.layoutMode = 'HORIZONTAL'
-                frame.horizontalPadding = 40
-                frame.counterAxisAlignItems = 'CENTER'
-                frame.appendChild(rectangleObject)
             }
         }
 
-        const link = await figma.createText()
+        const log = await figma.createText()
         await figma.loadFontAsync({family: "Inter", style: "Regular"})
-        link.x = 50
-        link.y = 0
-        link.characters = (`Количество обработанных имён: ${pluArray.length}. \nКоличество обработанных ссылок: ${linksArray.length}.`)
-        link.fontSize = 18
-        link.fills = [{type: 'SOLID', color: {r: 0, g: 0, b: 0}}]
+        log.x = 50
+        log.y = 0
+        log.characters = (`Количество обработанных имён: ${pluArray.length}. \nКоличество обработанных ссылок: ${linksArray.length}.`)
+        log.fontSize = 18
+        log.fills = [{type: 'SOLID', color: {r: 0, g: 0, b: 0}}]
 
         await figma.closePlugin()
     })()
